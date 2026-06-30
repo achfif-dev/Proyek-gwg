4bc244946a7722bee7b09c83e4dc420b59cd2c43
achfif-dev <achfif@gmail.com>
2026-06-30 14:53:49 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GWGSuperApp from './GWG_SuperApp'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    if (confirm('Versi baru aplikasi tersedia. Muat ulang sekarang?')) {
+      window.location.reload()
+    }
+  },
+  onOfflineReady() {
+    console.log('Aplikasi siap dipakai offline.')
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
